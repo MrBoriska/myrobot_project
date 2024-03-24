9a36b0cc50577b573f16166b04b576250216f707
Lapin Boris S <mail@borislap.ru>
2024-03-25 00:23:01 +0300

    Added feedback from wheels (experimental)

--- src/myrobot_description/src/myrobot_driver.cpp
+++ src/myrobot_description/src/myrobot_driver.cpp
@@ -25,17 +25,22 @@ class MyRobotDriverNode : public rclcpp::Node
     rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr pub_js;
 
     rclcpp::TimerBase::SharedPtr main_process;
+    rclcpp::TimerBase::SharedPtr fb_lw_process;
+    rclcpp::TimerBase::SharedPtr fb_rw_process;
 
     int fd_lw_pwm;
     int fd_rw_pwm;
     int fd_lw_dir;
     int fd_rw_dir;
-    int fd_lw_int;
-    int fd_rw_int;
+    struct pollfd fd_lw_int;
+    struct pollfd fd_rw_int;
     int fd_w_brake;
 
     double wheel_l = 0.0;
     double wheel_r = 0.0;
+
+    double l_pos = 0.0;
+    double r_pos = 0.0;
 
 public:
     MyRobotDriverNode()
@@ -66,16 +71,18 @@ public:
             perror("Unable to open /sys/class/gpio/gpio70/value");
             exit(1);
         }
-        // fd_lw_int = open("/sys/class/gpio/gpio24/value", ,O_WRONLY|O_CLOEXEC);
-        // if (fd_lw_dir == -1) {
-        //     perror("Unable to open", "/sys/class/gpio/gpio24/value");
-        //     exit(1);
-        // }
-        // fd_rw_int = open("/sys/class/gpio/gpio24/value", ,O_WRONLY|O_CLOEXEC);
-        // if (fd_lw_dir == -1) {
-        //     perror("Unable to open", "/sys/class/gpio/gpio24/value");
-        //     exit(1);
-        // }
+        fd_lw_int.fd = open("/sys/class/gpio/gpio72/value", ,O_RDONLY);
+        fd_lw_int.events = POLLPRI|POLLERR;
+        if (fd_lw_int.fd == -1) {
+            perror("Unable to open", "/sys/class/gpio/gpio72/value");
+            exit(1);
+        }
+        fd_rw_int.fd = open("/sys/class/gpio/gpio74/value", ,O_RDONLY);
+        fd_rw_int.events = POLLPRI|POLLERR;
+        if (fd_rw_int.fd == -1) {
+            perror("Unable to open", "/sys/class/gpio/gpio74/value");
+            exit(1);
+        }
         fd_w_brake = open("/sys/class/gpio/gpio73/value", ,O_WRONLY|O_CLOEXEC);
         if (fd_lw_dir == -1) {
             perror("Unable to open /sys/class/gpio/gpio73/value");
@@ -87,6 +94,9 @@ public:
         pub_js = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10 /*rclcpp::SensorDataQoS()*/);
 
         main_process = this->create_wall_timer(0.02s, std::bind(&MyRobotDriverNode::process, this));
+
+        fb_lw_process = this->create_wall_timer(0.1s, std::bind(&MyRobotDriverNode::feedback_left_wheel, this));
+        fb_rw_process = this->create_wall_timer(0.1s, std::bind(&MyRobotDriverNode::feedback_right_wheel, this));
     }
 
     ~MyRobotDriverNode() {
@@ -120,12 +130,37 @@ public:
         dprintf(fd_rw_pwm,"%u",(unsigned int)roundf((1.0-fabs(wheel_r/max_v))*freq_p));
     }
 
+    void feedback_left_wheel() {
+        poll(&fd_lw_int, 1, 150); //poll(struct pollfd, max fd, timeout), timeout=-1 --> never
+        if(fd_lw_int.revents & POLLPRI || fd_lw_int.revents & POLLERR) {
+            char buf[1];
+            len = read(fd_lw_int.fd, buf, 1);  //mandatory to make system register interrupt as served
+            printf("LW interrupt! %d\n", int(buf[0]));
+            l_pos += sgn(wheel_l);
+        }
+        lseek(fd_lw_int.fd, 0, 0); //return cursor to beginning of file or next read() will return EOF
+    }
+
+    void feedback_right_wheel() {
+        poll(&fd_rw_int, 1, 150); //poll(struct pollfd, max fd, timeout), timeout=-1 --> never
+        if(fd_rw_int.revents & POLLPRI || fd_rw_int.revents & POLLERR) {
+            char buf[1];
+            len = read(fd_rw_int.fd, buf, 1);  //mandatory to make system register interrupt as served
+            printf("RW interrupt! %d\n", int(buf[0]));
+            r_pos += sgn(wheel_r);
+        }
+        lseek(fd_rw_int.fd, 0, 0); //return cursor to beginning of file or next read() will return EOF
+    }
+
 };
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MyRobotDriverNode>());
+    auto  control_node = std::make_shared<MyRobotDriverNode>();
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(control_node);
+    executor.spin();
     rclcpp::shutdown();
     return 0;
 }
